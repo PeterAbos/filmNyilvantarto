ccae5fd5f49d6bff93c4c394c449bad155887d05
PeterAbos <abos.petike@gmail.com>
2025-10-20 14:16:32 +0200

    create routing

---
+++ app/Routing/Router.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Routing;
+
+use App\Controllers\HomeController;
+use App\Views\Display;
+
+class Router
+{
+    public function handle(): void
+    {
+        $method = strtoupper($_SERVER['REQUEST_METHOD']);
+        $requestUri = $_SERVER['REQUEST_URI'];
+
+        if ($method === 'POST' && isset($_POST['_method'])) {
+            $method = strtoupper($_POST['_method']);
+        }
+
+        $this->dispatch($method, $requestUri);
+    }
+
+    private function dispatch(string $method, string $requestUri): void
+    {
+        switch($method) {
+            case 'GET':
+                $this->handleGetRequests($requestUri);
+                break;
+            case 'POST':
+                $this->handlePostRequests($requestUri);
+                break;
+            case 'PATCH':
+                $this->handlePatchRequests($requestUri);
+                break;
+            case 'DELETE':
+                $this->handleDeleteRequests($requestUri);
+                break;
+            default:
+                $this->methodNotAllowed();
+        }
+    }
+
+    private function handleGetRequests(mixed $requestUri) {
+
+    }
+
+    private function handlePostRequests(mixed $requestUri) {
+        
+    }
+
+    private function handlePatchRequests(mixed $requestUri) {
+        
+    }
+
+    private function handleDeleteRequests(mixed $requestUri) {
+        
+    }
+
+    private function methodNotAllowed(): void
+    {
+        header ($_SERVER['SERVER_PROTOCOL'] . ' 405 Method Not Allowed');
+        Display::message("405 Method Not Allowed");
+    }
+    private function filterPostData(array $data): array
+    {
+        // Remove unnecessary keys in a clean and simple way
+        $filterKeys = ['_method', 'submit', 'btn-del', 'btn-save', 'btn-edit', 'btn-plus', 'btn-update'];
+        return array_diff_key($data, array_flip($filterKeys));
+    }
+    private function notFound(): void
+    {
+        header ($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+        Display::message("404 Not Found");
+    }
+
+
+}
