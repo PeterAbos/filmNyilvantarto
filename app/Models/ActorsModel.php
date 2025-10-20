df382401733bbac315054657318e471ba71e22c3
PeterAbos <abos.petike@gmail.com>
2025-10-20 13:23:48 +0200

    actors model2

--- app/Models/ActorsModel.php
+++ app/Models/ActorsModel.php
@@ -5,12 +5,14 @@ namespace App\Models;
 class ActorsModel extends Model {
 
     public string $name;
+    public string $birth_date;
 
     protected static $table = 'actors';
 
-    function __construct(string $name)
+    function __construct(string $name, string $birth_date)
     {
         parent::__construct();
         $this->name = $name;
+        $this->birth_date = $birth_date;
     }
 }
